ace56d18bf716e359e20581ea3ac316058452496
zak-dev-art <zachndungu861@gmail.com>
2025-11-26 14:37:03 +0300

    modified files

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+def return_evens(sequence):
+   
+    
+    return [num for num in sequence if num % 2 == 0]
 
-def return_evens(num_list):
-    pass
 
-def make_exclamation(sentence_list):
-    pass
+def make_exclamation(sentences):
+   
+    return [sentence + "!" for sentence in sentences]
